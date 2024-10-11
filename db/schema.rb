712748685158cb0b3f63370dd981cf8d353556c2
sushmitha ravindranath <sushmitha0614@gmail.com>
2024-10-11 17:40:27 +0530

    feat: add migration file, group model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_10_095757) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_10_162917) do
+  create_table "group_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["user_id"], name: "index_group_users_on_user_id"
+  end
+
+  create_table "groups", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "group_name"
+    t.text "description"
+    t.string "group_type"
+    t.string "currency"
+    t.boolean "IsSimplifyDebtEnabled", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,4 +37,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_10_095757) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
 end
