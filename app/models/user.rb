6708f3dfea124f373f201d018252f22c2d97e83d
sushmitha ravindranath <sushmitha0614@gmail.com>
2024-10-11 22:55:19 +0530

    feat: Added group_user model, association, add_user method & route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    has_many :group_users
+    has_many :groups, through: :group_users
+    
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Please provide a valid email id" }   
 end
